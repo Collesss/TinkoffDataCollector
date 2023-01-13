73976414c856a2626215d3f28c3528881848a443
Collesss <sergey_lesikov@mail.ru>
2023-01-14 00:25:41 +0300

    update

--- TinkoffDataCollectorService/Interfaces/ITinkoffDataCollectorService.cs
+++ TinkoffDataCollectorService/Interfaces/ITinkoffDataCollectorService.cs
@@ -2,6 +2,13 @@
 {
     public interface ITinkoffDataCollectorService
     {
+        /// <summary>
+        /// Service save data about candles
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <exception cref="Exceptions.TinkoffDataCollectorServiceException"></exception>
+        /// <exception cref="OperationCanceledException"></exception>
+        /// <returns>Task</returns>
         Task Run(CancellationToken cancellationToken);
     }
 }
